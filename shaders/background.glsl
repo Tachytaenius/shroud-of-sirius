656c24aefd6ff48ce2aaa9786c1af76c5089d8f2
Tachytaenius <henryfthomson@gmail.com>
2024-03-08 20:22:10 +0000

    Do sky unprojection per vertex (was per pixel)

--- shaders/background.glsl
+++ shaders/background.glsl
@@ -1,7 +1,29 @@
 // This shader must be loaded with the lib/simplex3d and sky include glsl files concatenated to the beginning, simplex3d first
 
-uniform float time;
+varying vec3 directionPreNormalize;
+varying vec2 modelSpaceXY;
+
+#ifdef VERTEX
+
 uniform mat4 clipToSky;
+
+vec4 position(mat4 loveTransform, vec4 modelSpacePos) {
+	// This solution to get the direction was figured out by me
+	directionPreNormalize = (
+		clipToSky * vec4( // clipToSky is inverse(perspectiveProjectionMatrix * cameraMatrixAtOriginWithCameraOrientation), the vec4 is a position in clip space, and the result of the multiplication is a position on a plane slice through the view frustum in world space (except camera translation is ignored)
+			VertexTexCoord.xy * 2.0 - 1.0, // Clip space x and y go from -1 to 1, not 0 to 1
+			-1.0, // This can actually be anything, all it defines is the length; we normalise anyway. I pick -1 because then it lands on the near plane which is what I was going for originally, though it could be the far plane (1) or whatever.
+			1.0 // Appears to affect FOV for the background. 0.5 is an increased FOV and 2.0 is a decreased FOV
+		)
+	).xyz; // No need to divide by w, but if we did the varying would be a position on the near plane
+	return loveTransform * modelSpacePos;
+}
+
+#endif
+
+#ifdef PIXEL
+
+uniform float time;
 uniform vec3 starDirection;
 uniform vec3 starColour;
 uniform float starAngularRadius;
@@ -9,16 +31,7 @@ uniform float starHaloAngularRange;
 uniform float skyStarColourMultiplier;
 
 vec4 effect(vec4 colour, sampler2D image, vec2 textureCoords, vec2 windowCoords) {
-	// This solution to get the direction was figured out by me
-	vec3 direction = normalize(
-		(
-			clipToSky * vec4( // clipToSky is inverse(perspectiveProjectionMatrix * cameraMatrixAtOriginWithCameraOrientation), the vec4 is a position in clip space, and the result of the multiplication is a position on a plane slice through the view frustum in world space (except camera translation is ignored)
-				textureCoords * 2.0 - 1.0,
-				-1.0, // This can actually be anything, all it defines is the length; we normalise anyway. I pick -1 because then it lands on the near plane which is what I was going for originally, though it could be the far plane (1) or whatever.
-				1.0 // Appears to affect FOV for the background. 0.5 is an increased FOV and 2.0 is a decreased FOV
-			)
-		).xyz
-	);
+	vec3 direction = normalize(directionPreNormalize);
 	return vec4(sampleSky(
 		direction,
 		time,
@@ -29,3 +42,5 @@ vec4 effect(vec4 colour, sampler2D image, vec2 textureCoords, vec2 windowCoords)
 		skyStarColourMultiplier
 	), 1.0);
 }
+
+#endif
