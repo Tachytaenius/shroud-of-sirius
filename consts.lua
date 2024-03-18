8a683583a1b6a8cbda3c398b84fe4e73f762795f
Tachytaenius <henryfthomson@gmail.com>
2024-03-18 22:46:17 +0000

    Change near and far plane distances

--- consts.lua
+++ consts.lua
@@ -11,8 +11,8 @@ consts.vertexFormat = {
 }
 consts.frontFaceWinding = "cw"
 consts.meshCullMode = "back"
-consts.farPlaneDistance = 1000
-consts.nearPlaneDistance = 0.01
+consts.farPlaneDistance = 10000
+consts.nearPlaneDistance = 0.1
 -- Export OBJs from Blender with +Y up and +Z forward
 consts.loadObjCoordMultiplier = vec3(1, 1, -1) -- TODO: Why?
 
